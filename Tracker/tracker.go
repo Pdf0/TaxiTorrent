b96203ba396f79dc94108ea0ca7c7f746cda5df9
PdF0 <pedro.er.sousa@gmail.com>
2023-11-09 11:54:15 +0000

    Fixed sending multiple messages in the same connection

--- Tracker/tracker.go
+++ Tracker/tracker.go
@@ -40,23 +40,28 @@ func main() {
 }
 
 func processClient(connection net.Conn, dataBase map[string][]CentralProtocol.File) {
+	defer connection.Close()
 
-	buffer := make([]byte, 1024)
-	mLen, err := connection.Read(buffer)
+	for {
+		buffer := make([]byte, 1024)
+		mLen, err := connection.Read(buffer)
 
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+
+		if mLen == 0 {
+			break
+		}
+
+		s := new(CentralProtocol.SYN)
+		util.DecodeToStruct(buffer[:mLen], s)
+
+		fullAddr := net.JoinHostPort(s.Ip.String(), fmt.Sprintf("%d", s.Port))
+		dataBase[fullAddr] = s.FileList
+
+		fmt.Println("Received: ", *s)
+		fmt.Println(dataBase)
+		_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
 	}
-
-	s := new(CentralProtocol.SYN)
-	util.DecodeToStruct(buffer[:mLen], s)
-
-	fullAddr := net.JoinHostPort(s.Ip.String(), fmt.Sprintf("%d", s.Port))
-	dataBase[fullAddr] = s.FileList
-
-	fmt.Println("Received: ", *s)
-	fmt.Println(dataBase)
-	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
-	connection.Close()
-
 }
