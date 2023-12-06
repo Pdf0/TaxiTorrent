919093879603e281d51da8f325c53ac5b36af266
hgp22 <henriquepereira31337@gmail.com>
2023-12-06 20:04:15 +0000

    Add a script that sets up the topology for testing

---
+++ setup.sh
@@ -0,0 +1,2 @@
+xfce4-terminal --command=" vcmd -c /tmp/pycore.39593/Servidor1 -- bash -i -c 'correbind9'" &
+xfce4-terminal --command="vcmd -c /tmp/pycore.39593/Servidor2 -- bash -i -c 'tracker'" &
